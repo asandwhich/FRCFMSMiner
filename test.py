43f46fc3deda1a1a514f5bf83e6cab9b9ab300c1
Eric Osburn <bjonesen@gmail.com>
2013-03-06 22:37:09 -0600

    Updated data format, Added new functionality, Try it, It's neat!

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ from string import replace
 from bs4 import BeautifulSoup
 import json
 
-teams = {}
+teamq = {}
+teamqp = {}
+teamp = {}
 
 page = urllib2.urlopen("http://www2.usfirst.org/2013comp/Events/NHMA/rankings.html")
 soup = BeautifulSoup(page.read())
@@ -17,7 +19,9 @@ row = temp.find_all('tr')[2]
 
 while row != None:
 	data = row.contents
-	teams[unicode(data[3].contents[0])]=[float(), float(unicode(data[5].contents[0])),float(unicode(data[7].contents[0])),float(unicode(data[9].contents[0])),float(unicode(data[11].contents[0])),float(unicode(data[13].contents[0])),float(unicode(data[17].contents[0])),float(unicode(data[19].contents[0]))] 
+	teamq[unicode(data[3].contents[0])]=[float(unicode(data[5].contents[0])),float(unicode(data[7].contents[0]))]
+	teamqp[unicode(data[3].contents[0])]=[float(), float(unicode(data[5].contents[0])), float(unicode(data[19].contents[0]))]
+	teamp[unicode(data[3].contents[0])]=[float(), float(unicode(data[19].contents[0]))]
 	row = row.next_sibling.next_sibling
 
 page = urllib2.urlopen("http://www2.usfirst.org/2013comp/Events/NHMA/matchresults.html")
@@ -30,17 +34,35 @@ row = temp.find_all('tr')[3]
 
 while row != None:
 	data = row.contents
-	teams[unicode(data[5].contents[0])][0] += float(unicode(data[17].contents[0]))
-	teams[unicode(data[7].contents[0])][0] += float(unicode(data[17].contents[0]))
-	teams[unicode(data[9].contents[0])][0] += float(unicode(data[17].contents[0]))
-	teams[unicode(data[11].contents[0])][0] += float(unicode(data[19].contents[0]))
-	teams[unicode(data[13].contents[0])][0] += float(unicode(data[19].contents[0]))
-	teams[unicode(data[15].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamqp[unicode(data[5].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamqp[unicode(data[7].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamqp[unicode(data[9].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamqp[unicode(data[11].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamqp[unicode(data[13].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamqp[unicode(data[15].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamp[unicode(data[5].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamp[unicode(data[7].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamp[unicode(data[9].contents[0])][0] += float(unicode(data[17].contents[0]))
+	teamp[unicode(data[11].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamp[unicode(data[13].contents[0])][0] += float(unicode(data[19].contents[0]))
+	teamp[unicode(data[15].contents[0])][0] += float(unicode(data[19].contents[0]))
 	row = row.next_sibling.next_sibling
 
-for key in teams:
-	teams[key][0] /= teams[key][7]
+for key in teamqp:
+	teamqp[key][0] /= teamqp[key][2]
+
+for key in teamp:
+	teamp[key][0] /= teamp[key][1]
 
 f = open('data.js', 'w')
-f.write(replace(replace(replace(replace(json.dumps(teams, indent=1),'\": [', ','), '\"', '['), '{', '['), '}', ']'))
+f.write("var teamq = ")
+f.write(replace(replace(replace(replace(json.dumps(teamq, indent=1),'\": [', ','), '\"', '['), '{', '['), '}', ']'))
+f.write("\n\n")
+
+f.write("var teamqp = ")
+f.write(replace(replace(replace(replace(json.dumps(teamqp, indent=1),'\": [', ','), '\"', '['), '{', '['), '}', ']'))
+f.write("\n\n")
+
+f.write("var teamp = ")
+f.write(replace(replace(replace(replace(json.dumps(teamp, indent=1),'\": [', ','), '\"', '['), '{', '['), '}', ']'))
 f.close()
